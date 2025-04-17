066c9f0e6c21ec3a06394dfe987550e1698dafbd
Hreeth Gupta <hreethgupta2006@gmail.com>
2025-04-17 17:33:30 +0530

    changed window options

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,19 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::{thread, time::Duration};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            let window = tauri::Manager::get_webview_window(app, "main").unwrap();
+      
+            thread::spawn(move || {
+                thread::sleep(Duration::from_millis(8000));
+                let _ = window.set_fullscreen(true);
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
